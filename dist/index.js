5b6f036e854a40ac6c6609102cffc402c928e52a
PowellGuy <jordan.marcus.powell@gmail.com>
2026-03-10 23:23:49 +0000

    update -

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function submit_Btn() {
     const creator_name = document.querySelector("#creator_Name");
     const creator_URL = document.querySelector("#creator_URL");
-    const creator_Category = document.querySelector("#category_Type");
+    const creator_Category = document.querySelector("#creator_Type");
+    const myElement = document.querySelector("#preview_Name");
+    myElement.innerHTML;
     let creator = [0, creator_name.value, creator_URL.value, creator_Category.value];
     alert(creator);
 }
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ function submit_Btn()
 {
     const creator_name = document.querySelector("#creator_Name") as HTMLInputElement;
     const creator_URL = document.querySelector("#creator_URL") as HTMLInputElement;
-    const creator_Category = document.querySelector("#category_Type") as HTMLSelectElement;
+    const creator_Category = document.querySelector("#creator_Type") as HTMLSelectElement;
+    
+    const myElement = document.querySelector("#preview_Name") as HTMLInputElement;
+    myElement.innerHTML;
 
-    let  creator: [number, string, string, string]  = [0, creator_name.value, creator_URL.value, creator_Category.value];
+    let creator: [number, string, string, string]  = [0, creator_name.value, creator_URL.value, creator_Category.value];
 
     alert(creator);
- 
 }

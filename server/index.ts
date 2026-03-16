cba00a6f9a5f703a6f0535e56967b802b0a24447
PowellGuy <jordan.marcus.powell@gmail.com>
2026-03-16 23:17:42 +0000

    update - 16th march

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,28 @@
+import 'dotenv/config';
+import express from "express";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express()
+const port = 3000
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..')));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(port, () =>
+{
+    console.log(`running on port: ${port}`);
+});
+
+app.get("/", (req, res) =>
+{
+    res.sendFile(path.join(__dirname, '..', '/home_page.html'));
+});
+
 
 
 function preview_Btn()
